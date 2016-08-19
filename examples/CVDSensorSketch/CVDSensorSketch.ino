81a53b4362b2f87827f73b89bad23628aea747e3
Admar Schoonen <admar@familieschoonen.nl>
2016-08-19 22:19:40 +0200

    Initial commit

---
+++ examples/CVDSensorSketch/CVDSensorSketch.ino
@@ -0,0 +1,73 @@
+#include <CVDSensor.h>
+
+/*
+ * CVDSense Library Demo Sketch
+ * Admar Schoonen 2016
+ * Connect an electrode (piece of metal sheet / foil) to an analog pin
+ */
+
+/* 
+ * Number of capacitive sensors. Needs a minimum of 2. When using only one
+ * sensor, set N_SENSORS to 2 and use an unused analog input pin for the second
+ * sensor.
+ */
+#define N_SENSORS			2
+
+/* 
+ * Number of measurements per sensor to take in one cycle. More measurements
+ * means more noise reduction / spreading, but is also slower.
+ */
+#define N_MEASUREMENTS_PER_SENSOR	16
+
+/* cvdSensors is the actual object that contains all the sensors */
+CvdSensors<N_SENSORS, N_MEASUREMENTS_PER_SENSOR> cvdSensors;
+
+void setup()                    
+{
+	Serial.begin(115200);
+
+	cvdSensors.data[0].pin = 0; /* Analog pin 0 */
+	cvdSensors.data[0].enableSlewrateLimiter = true;
+	//cvdSensors.data[0].sampleTypeNormal;
+
+	cvdSensors.data[1].pin = 2; /* Analog pin 2 */
+	cvdSensors.data[1].enableSlewrateLimiter = true;
+	//cvdSensors.data[1].sampleTypeNormal;
+
+	cvdSensors.printScanOrder();
+}
+
+void loop()                    
+{
+	static int prev = millis();
+	int now;
+
+	cvdSensors.sample();
+	Serial.print("raw[0]: ");
+	Serial.print(cvdSensors.data[0].raw);
+	Serial.print(", \tavg: ");
+	Serial.print(cvdSensors.data[0].avg);
+	Serial.print(", \tdelta: ");
+	Serial.print(cvdSensors.data[0].delta);
+	Serial.print(", \tstate: ");
+	Serial.print(cvdSensors.data[0].buttonState);
+	Serial.print(", \tcounter: ");
+	Serial.print(cvdSensors.data[0].counter);
+
+	Serial.print("; \t\traw[1]: ");
+	Serial.print(cvdSensors.data[1].raw);
+	Serial.print(", \tavg: ");
+	Serial.print(cvdSensors.data[1].avg);
+	Serial.print(", \tdelta: ");
+	Serial.print(cvdSensors.data[1].delta);
+	Serial.print(", \tstate: ");
+	Serial.print(cvdSensors.data[1].buttonState);
+	Serial.print(", \tcounter: ");
+	Serial.print(cvdSensors.data[1].counter);
+	Serial.print("; \tLoop time: ");
+	now = millis();
+	Serial.println(now - prev);
+	prev = now;
+
+	//delay(100);
+}
