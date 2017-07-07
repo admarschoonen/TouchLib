0e8f9b826c5bc099bd791b3d56f6c609bdf9de05
Admar Schoonen <admar@familieschoonen.nl>
2017-07-07 16:15:54 +0200

    Fixed capacitive + resistive example

--- examples/Example12CombiningResistiveAndCapacitiveSensing/Example12CombiningResistiveAndCapacitiveSensing.ino
+++ examples/Example12CombiningResistiveAndCapacitiveSensing/Example12CombiningResistiveAndCapacitiveSensing.ino
@@ -1,4 +1,3 @@
-#include <EEPROM.h>
 #include <CVDSensor.h>
 
 /*
@@ -8,10 +7,19 @@
  */
 
 /* 
- * Number of capacitive sensors. Needs a minimum of 2. When using only one
- * sensor, set N_SENSORS to 2 and use an unused analog input pin for the second
- * sensor. For 2 or more sensors you don't need to add an unused analog input.
+ * To make 1 set of capacitive + resistive sensors:
+ *
+ * Take 2 pieces of conductive fabric and 1 piece of ESD foam or velostat.
+ * Attach the conductive fabric to the foam / velostat; make sure not to put too
+ * much tension on the foam / velostat or performance of the resistive sensor
+ * will be very limited.
+ *
+ * Attach one electrode to A0 and the other electrode to pin 2.
+ *
+ * Repeat this again for a second set of capacitive + resistive sensors; connect
+ * this to A1 and (again) pin 2.
  */
+
 #define N_SENSORS			4
 
 /* 
@@ -45,46 +53,54 @@ void setup()
 	Serial.println();
 	Serial.println();
 
-	cvdSensors.data[0].pin = A0; /* Analog pin 0 */
-	cvdSensors.data[0].setParallelCapacitanceManually = false;
+	/* 
+	 * By default, capacitive sensors on A0 - A3 are assumed. Thus, for the
+	 * capacitive part of our sensors (sensors 0 and 1) we don't need to
+	 * change the pin settings or sample method.
+	 *
+	 * However, we do want to automatically trigger a recalibration of the
+	 * resistive sensor if the capacitive sensor is released.
+	 */
 
-	cvdSensors.data[1].pin = A1; /* Analog pin 1 */
-	cvdSensors.data[1].setParallelCapacitanceManually = false;
+	cvdSensors.data[0].forceCalibrationWhenApproachingFromPressed = 
+		(1 << 2); // Recalibrate sensor 2 if sensor 0 is released
 
-	cvdSensors.data[2].pin = A3; /* Analog pin 3 */
-	cvdSensors.data[2].setParallelCapacitanceManually = false;
-	//cvdSensors.data[2].releasedToApproachedThreshold = 400.0;
-	//cvdSensors.data[2].approachedToReleasedThreshold = 200.0;
-	cvdSensors.data[2].approachedToPressedThreshold = 1400.0;
-	cvdSensors.data[2].pressedToApproachedThreshold = 1300.0;
+	cvdSensors.data[1].forceCalibrationWhenApproachingFromPressed = 
+		(1 << 3); // Recalibrate sensor 3 if sensor 1 is released
 
 	/*
-	 * When this button switches from pressed-to-approached to approached,
-	 * recalibrate button 3 to reduce impact of hysteresis in the foam.
+	 * The resistive part of our sensors are sensors 2 and 3. By default,
+	 * the library assumes these are capacitive sensors connected to A2 and
+	 * A3. However, for our capacitive + resistive sensors, they are
+	 * connected to A0 and A1.
+	 *
+	 * The other side of the resistive part of the sensor is connected to
+	 * pin 2 respectively pin 3.
 	 */
-	cvdSensors.data[2].forceCalibrationWhenApproachingFromPressed = 
-		(1 << 3);
+	cvdSensors.data[2].pin = A0;
+	cvdSensors.data[2].sampleMethodResistive_gndPin = 2;
+	cvdSensors.data[2].sampleMethod = TLSampleMethodResistive;
 
-	/*
-	 * When this button switches from approached-to-released to released,
-	 * recalibrate this button to reduce impact of hysteresis in the foam.
-	 */
-	//cvdSensors.data[2].forceCalibrationWhenReleasingFromApproached =
-	//	(1 << 2);
-
-	cvdSensors.data[3].pin = A3; /* Analog pin 3 */
+	cvdSensors.data[3].pin = A1;
+	cvdSensors.data[3].sampleMethodResistive_gndPin = 3;
 	cvdSensors.data[3].sampleMethod = TLSampleMethodResistive;
-	cvdSensors.data[3].sampleMethodResistive_gndPin = 2;
 
-	cvdSensors.printScanOrder();
+	/* We also need to adjust the thresholds for the resistive sensors. */
+	cvdSensors.data[2].releasedToApproachedThreshold = 100.0;
+	cvdSensors.data[2].approachedToReleasedThreshold = 80.0;
+	cvdSensors.data[2].approachedToPressedThreshold = 300.0;
+	cvdSensors.data[2].pressedToApproachedThreshold = 240.0;
+
+	cvdSensors.data[3].releasedToApproachedThreshold = 100.0;
+	cvdSensors.data[3].approachedToReleasedThreshold = 80.0;
+	cvdSensors.data[3].approachedToPressedThreshold = 300.0;
+	cvdSensors.data[3].pressedToApproachedThreshold = 240.0;
 }
 
 void loop()
 {
-	static int prev = millis();
-	int now;
-
 	cvdSensors.sample();
+
 	Serial.print("raw[0]: ");
 	Serial.print(cvdSensors.getRaw(0));
 	Serial.print(", \tdelta: ");
@@ -112,6 +128,4 @@ void loop()
 	Serial.print(cvdSensors.getDelta(3));
 	Serial.print(", \tstate: ");
 	Serial.println(cvdSensors.getState(3));
-
-	//delay(100);
 }
