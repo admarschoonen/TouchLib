f0e6f01e003fdd34bd24d369665efd00230a9732
Admar Schoonen <admar@familieschoonen.nl>
2017-07-12 15:34:55 +0200

    Debugged printing of floating point numbers

--- examples/Example02SignalToNoiseRatio/Example02SignalToNoiseRatio.ino
+++ examples/Example02SignalToNoiseRatio/Example02SignalToNoiseRatio.ino
@@ -46,7 +46,18 @@ void setup()
 	Serial.println();
 	Serial.println();
 
+	tlSensors.initialize(0, TLSampleMethodCVD);
+	tlSensors.initialize(1, TLSampleMethodCVD);
+	tlSensors.initialize(2, TLSampleMethodResistive);
+	tlSensors.initialize(3, TLSampleMethodResistive);
+
+	tlSensors.data[2].tlStructSampleMethod.resistive.pin = A0;
+	tlSensors.data[2].tlStructSampleMethod.resistive.gndPin = 2;
+	tlSensors.data[3].tlStructSampleMethod.resistive.pin = A1;
+	tlSensors.data[3].tlStructSampleMethod.resistive.gndPin = 3;
+
 	for (n = 0; n < N_SENSORS; n++) {
+
 		/*
 		 * Disable state machine so we can control updating average in
 		 * the main loop.
@@ -92,29 +103,118 @@ int processSerialData(int b)
 	return n;
 }
 
-void floatToIntFrac(float f, int scale, int precision, int * iInt, int * iFrac)
+void floatToIntFrac(float f, uint32_t precision, char *sign, uint32_t * iInt,
+		uint32_t * iFrac)
 {
 	float fFrac;
 
-	if (f > 0) {
-		*iInt = scale * f;
-		fFrac = scale * f - *iInt;
+	if (f >= 0) {
+		*iInt = f;
+		fFrac = f - *iInt;
+		*sign = ' ';
 	} else {
-		*iInt = scale * f + 1;
-		fFrac = 1 - (scale * f - *iInt);
+		*iInt = -(f);
+		fFrac = -(f + *iInt);
+		*sign = '-';
 	}
-	*iFrac = floor(pow(10, precision - 1) * fFrac);
+
+	if (precision > 0) {
+		*iFrac = floor(pow(10, precision) * fFrac);
+	} else {
+		*iFrac = 0;
+	}
 }
+
+uint32_t intLength(uint32_t i)
+{
+	uint32_t l;
+	uint32_t tmp;
+	
+	if (i < 0) {
+		tmp = -i;
+	} else {
+		tmp = i;
+	}
+	
+	l = 1;
+	while (tmp >= 10) {
+		l++;
+		tmp = tmp / 10;
+	}
+	
+	return l;
+}
+
+int floatToStr(float f, int scale, int precision, char *s, int length)
+{
+	char sign;
+	uint32_t iInt;
+	uint32_t iFrac;
+	uint32_t i;
+	uint32_t iLength, fLength;
+	uint32_t tmp;
+	
+	floatToIntFrac(f, precision, &sign, &iInt, &iFrac);
+	iLength = intLength(iInt);
+	fLength = intLength(iFrac);
+	
+	if (scale < iLength) {
+		i = snprintf(s, length - 1, "OVF");
+	} else {
+		i = 0;
+		if (i < length - 1) {
+			/* print spaces */
+			tmp = (length - 1 - i < scale - iLength) ? length - 1 - i : scale - iLength;
+			memset(&(s[i]), ' ', tmp);
+			i += tmp;
+			if (i < length - 1) {
+				/* print sign */
+				s[i++] = sign;
+				if (i < length - 1) {
+					/* print integer portion */
+					i += snprintf(&(s[i]), length - 1 - i, "%lu", iInt);
+					if (i < length - 1) {
+						/* print dot */
+						s[i++] = '.';
+						if (i < length - 1) {
+							/* print leading 0's for decimal portion */
+							tmp = (length - 1 - i < precision - fLength) ?
+								length - 1 - i : precision - fLength;
+							tmp = (tmp < 0) ? 0 : tmp;
+							memset(&(s[i]), '0', tmp);
+							i += tmp;
+							if (i < length - 1) {
+								/* print decimal portion */
+								i += snprintf(&(s[i]), length - 1 - i, "%lu", iFrac);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return i;
+}
+
+
 
 void loop()
 {
 	static int b = 0;
 	char s[200] = {'\0'};
+	char rawS[13] = {'\0'};
+	char valueS[13] = {'\0'};
+	char avgS[13] = {'\0'};
+	char deltaS[13] = {'\0'};
+	char noisePowerS[13] = {'\0'};
+	char noiseAmpS[13] = {'\0'};
+	char snrS[13] = {'\0'};
 	int n;
 	const char *buttonStateLabel;
-	float avg, delta, noisePower, noiseAmp, snr;
-	int avgInt, avgFrac, deltaInt, deltaFrac, noisePowerInt, noisePowerFrac,
-		noiseAmpInt, noiseAmpFrac, snrInt, snrFrac;
+	float raw, value, avg, delta, noisePower, noiseAmp, snr;
+	int rawInt, rawFrac, valueInt, valueFrac, avgInt, avgFrac, deltaInt,
+		deltaFrac, noisePowerInt, noisePowerFrac, noiseAmpInt,
+		noiseAmpFrac, snrInt, snrFrac;
 
 	n = processSerialData(b);
 
@@ -131,6 +231,8 @@ void loop()
 		b = n;
 	}
 
+	raw = tlSensors.getRaw(b);
+	value = tlSensors.getValue(b);
 	avg = tlSensors.getAvg(b);
 	delta = tlSensors.getDelta(b);
 	noisePower = tlSensors.data[b].noisePower;
@@ -138,15 +240,16 @@ void loop()
 	snr = 10 * log10(delta * delta / noisePower);
 	buttonStateLabel = tlSensors.getStateLabel(b);
 
-	floatToIntFrac(avg, 1, 2, &avgInt, &avgFrac);
-	floatToIntFrac(delta, 1, 2, &deltaInt, &deltaFrac);
-	floatToIntFrac(noisePower, 1, 2, &noisePowerInt, &noisePowerFrac);
-	floatToIntFrac(noiseAmp, 1, 2, &noiseAmpInt, &noiseAmpFrac);
-	floatToIntFrac(snr, 1, 2, &snrInt, &snrFrac);
+	floatToStr(raw, 6, 3, rawS, sizeof(rawS));
+	floatToStr(value, 6, 3, valueS, sizeof(valueS));
+	floatToStr(avg, 6, 3, avgS, sizeof(avgS));
+	floatToStr(delta, 6, 3, deltaS, sizeof(deltaS));
+	floatToStr(noisePower, 6, 3, noisePowerS, sizeof(noisePowerS));
+	floatToStr(noiseAmp, 6, 3, noiseAmpS, sizeof(noiseAmpS));
+	floatToStr(snr, 6, 3, snrS, sizeof(snrS));
 
-	snprintf(s, sizeof(s) -1, "button[%d]: avg: % 7d.%02d, delta: "
-		"% 7d.%02d, noise: % 7d.%02d, SNR: % 7d.%02d dB, state: %s\r\n",
-		b, avgInt, avgFrac, deltaInt, deltaFrac, noiseAmpInt,
-		noiseAmpFrac, snrInt, snrFrac, buttonStateLabel);
+	snprintf(s, sizeof(s) -1, "button[%d]: raw: %s, value: %s, avg: %s, "
+		"delta: %s, noise: %s, " "SNR: %s dB, state: %s\r\n", b, rawS,
+		valueS, avgS, deltaS, noiseAmpS, snrS, buttonStateLabel);
 	Serial.print(s);
 }
